120c1cb30ed76e9d8db9e9e6bc8a1b2542442b5c
EkaterinaPoddubskaya <kpoddubskaya@xbsoftware>
2024-08-08 19:08:41 +0300

    fix defects

--- main.js
+++ main.js
@@ -8,11 +8,19 @@ const header = {
         {
             view:"button", 
             label:"Profile", 
-            id: "myprofile",
+            id: "profileButton",
             type: "icon", 
             icon: "wxi-user",
             autowidth: true,
-            css: "webix_transparent header_button"
+            css: "webix_transparent header_button",
+            popup: {
+                view:"popup",
+                body: {
+                    view:"list", 
+                    data:[ "Settings", "Log out" ],
+                    autoheight: true
+                }
+            }    
         }
     ],  
     padding: 5,
@@ -42,7 +50,7 @@ const sideMenuList = {
 
 const filmLibraryTable = {
     view:"datatable", 
-    id: "mytable",
+    id: "filmsTable",
     autoConfig: true,
     data: SMALL_FILM_SET,
     gravity: 3, 
@@ -51,33 +59,58 @@ const filmLibraryTable = {
 
 const editFilmsForm = {
     view:"form",
-    id: "myform",
+    id: "filmsForm",
     autoheight: false,
     minWidth: 200,
     elements:[
         { rows:[ 
             { template:"EDIT FILMS", type:"section" },
             { view:"text", label:"Title", name: "title", value:"", invalidMessage: "Title must not be empty" },
-            { view:"text", label:"Year", name: "year", value:"", invalidMessage: "Enter year between 1970 and " + new Date().getFullYear() },
+            { view:"text", label:"Year", name: "year", value:"", invalidMessage: `Enter year between 1970 and ${new Date().getFullYear()}` },
             { view:"text", label:"Rating", name: "rating", value:"", invalidMessage: "Rating can not be empty or equal to 0" },
             { view:"text", label:"Votes", name: "votes", value:"", invalidMessage: "Enter votes between 1 and 100000 exclusively" }
         ]},
         { cols:[
-            { view:"button", label:"Add new" , id:"myaddnew", css: "webix_primary" },
-            { view:"button", label:"Clear" , id:"myclear" }
+            { view:"button", label:"Add new" , id:"addNewButton", css: "webix_primary", 
+                click: () => {
+                    const formInput = $$("filmsForm").getValues();
+                    if ($$("filmsForm").validate()) {
+                        $$("filmsTable").add(formInput);
+                        $$("filmsForm").clear();
+                        webix.message({
+                            text: "Your film is successfully added to the list!",
+                            type: "success"
+                        });
+                    }
+                }
+            },
+            { view:"button", label:"Clear" , id:"clearButton",
+                click: () => {
+                    webix.confirm({
+                        title: "Form is about to be cleared",
+                        text: "Do you want to clear the form?"
+                    })
+                    .then(
+                        () => {
+                            $$("filmsForm").clear();
+                            $$("filmsForm").clearValidation();
+                        }
+                    );
+                }
+             }
         ],
             margin: 15
         }
     ],
     rules:{
         title: webix.rules.isNotEmpty,
-        year: function(value) {
+        year: value => {
             return (value >= 1970) && (value <= new Date().getFullYear());
         },
-        rating: function(value) {
+        rating: value => {
             return value != 0;
         },
-        votes: function(value) {
+        votes: value => {
             return (value > 0) && (value < 100000);
         }
     }
--- user-interaction.js
+++ user-interaction.js
@@ -1,52 +1,12 @@
-webix.ui({
-    view:"popup",
-    id: "mypopup",
-    body: {
-        view:"list", 
-        data:[ "Settings", "Log out" ],
-        autoheight: true
-    }
-});
-
-$$("myprofile").attachEvent("onItemClick", function() {
-    const profileNode = $$("myprofile").getNode();
-    $$("mypopup").show(profileNode);
-});
-
-const titleInput = $$("myform").elements.title;
-const yearInput = $$("myform").elements.year;
-const ratingInput = $$("myform").elements.rating;
-const votesInput = $$("myform").elements.votes;
-
-$$("myaddnew").attachEvent("onItemClick", function() {
-    if ($$("myform").validate()) {
-        const film = {
-            title: titleInput.getValue(),
-            year: yearInput.getValue(),
-            rating: ratingInput.getValue(), 
-            votes: votesInput.getValue()
-        }
-        $$('mytable').add(film);
-        webix.message({
-            text: "Your film is successfully added to the list!",
-            type: "success"
-        });
-    }
-});
-
-$$("myclear").attachEvent("onItemClick", function() {
-    webix.confirm({
-        title: "Form is about to be cleared",
-        text: "Do you want to clear the form?"
-    })
-    .then(
-        function(){
-            titleInput.setValue("");
-            yearInput.setValue("");
-            ratingInput.setValue("");
-            votesInput.setValue("");
-        
-            $$("myform").clearValidation();
-        }
-    );
-});
+// $$("clearButton").attachEvent("onItemClick", () => {
+//     webix.confirm({
+//         title: "Form is about to be cleared",
+//         text: "Do you want to clear the form?"
+//     })
+//     .then(
+//         () => {
+//             $$("filmsForm").clear();
+//             $$("filmsForm").clearValidation();
+//         }
+//     );
+// });
