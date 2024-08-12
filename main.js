b4faaecea9a713400eefc91b1f250d78702b4f8d
EkaterinaPoddubskaya <kpoddubskaya@xbsoftware>
2024-08-12 17:47:53 +0300

    add formats to form elements and change validation functions

--- main.js
+++ main.js
@@ -65,30 +65,124 @@ const editFilmsForm = {
     elements:[
         { rows:[ 
             { template:"EDIT FILMS", type:"section" },
-            { view:"text", label:"Title", name: "title", value:"", invalidMessage: "Title must not be empty" },
-            { view:"text", label:"Year", name: "year", value:"", invalidMessage: `Enter year between 1970 and ${new Date().getFullYear()}` },
-            { view:"text", label:"Rating", name: "rating", value:"", invalidMessage: "Rating can not be empty or equal to 0" },
-            { view:"text", label:"Votes", name: "votes", value:"", invalidMessage: "Enter votes between 1 and 100000 exclusively" }
+            { 
+                view:"text", label:"Title", name: "title", id: "title", value:"", 
+                format: {
+                    parse: (input) => { 
+                        return input.replace(/[<>]/g, '').replace(/\s+/g, ' ').trim();
+                    },
+                    edit: (input) => { 
+                        return input.replace(/[<>]/g, '').replace(/\s+/g, ' ').trim();
+                    }
+                },
+                invalidMessage: "Title must not be empty", 
+                validate: webix.rules.isNotEmpty 
+            },
+            { view:"text", label:"Year", name: "year", id: "year", value:"", 
+                format: {
+                    parse: (input) => { 
+                        let value = parseInt(input.toString().match(/[0-9]+/));
+                        if (value > 9999) {
+                            value = parseInt(value.toString().slice(0, 4));
+                        }
+                        if (value) {
+                            return value;
+                        }
+                        return NaN; 
+                    },
+                    edit: (input) => { 
+                        let value = parseInt(input.toString().match(/[0-9]+/));
+                        if (value > 9999) {
+                            value = parseInt(value.toString().slice(0, 4));
+                        }
+                        if (value) {
+                            return value;
+                        }
+                        return ""; 
+                    }
+                }, 
+                invalidMessage: "",
+                validate(value) {
+                    let invMessage = "Year must be a whole number";
+                    if (parseInt(value) == value) {
+                        if ((value >= 1970) && (value <= new Date().getFullYear())) {
+                            return true;
+                        }
+                        invMessage = `Enter year between 1970 and ${new Date().getFullYear()}`;
+                    } 
+                    $$("year").define("invalidMessage", invMessage);
+                    return false;
+                }
+            },
+            { 
+                view:"text", label:"Rating", name: "rating", id: "rating", value:"", 
+                format: {
+                    parse: (input) => { 
+                        const value = parseFloat(input.toString().match(/[0-9]+.?[0-9]*/));
+                        if (value) {
+                            return parseFloat(value.toFixed(2));
+                        }
+                        return NaN; 
+                    },
+                    edit: (input) => { 
+                        const value = parseFloat(input.toString().match(/[0-9]+.?[0-9]*/));
+                        if (value) {
+                            return parseFloat(value.toFixed(2));
+                        }
+                        return ""; 
+                    }
+                }, 
+                invalidMessage: "", 
+                validate(value) {
+                    let invMessage = "Rating can not be empty";
+                    if (webix.rules.isNumber(value)) {
+                        if (value > 0 && value <= 10) {
+                            return true;
+                        }
+                        invMessage = "Enter rating between 0 and 10";
+                    }
+                    $$("rating").define("invalidMessage", invMessage);
+                    return  false;
+                }
+            },
+            { 
+                view:"text", label:"Votes", name: "votes", id: "votes", value:"", 
+                format: {
+                    parse: (input) => { 
+                        let value = parseInt(input.toString().match(/[0-9]+/));
+                        if (value) {
+                            return value;
+                        }
+                        return NaN; 
+                    },
+                    edit: (input) => { 
+                        let value = parseInt(input.toString().match(/[0-9]+/));
+                        if (value) {
+                            return value;
+                        }
+                        return ""; 
+                    }
+                }, 
+                invalidMessage: "", 
+                validate(value) {
+                    let invMessage = "Votes must be a whole number";
+                    if (parseInt(value) == value) {
+                        if ((value > 0) && (value < 100000)) {
+                            return true;
+                        }
+                        invMessage = "Enter votes between 1 and 100000 exclusively";
+                    } 
+                    $$("votes").define("invalidMessage", invMessage);
+                    return false;
+                }
+            }
         ]},
         { cols:[
             { view:"button", label:"Add new" , id:"addNewButton", css: "webix_primary", 
                 click: () => {
                     const form = $$("filmsForm");
-                    if (form.validate()) {
-                        const { title ,year, rating, votes } = form.elements;
-                        
-                        const titleInput = title.getValue().replace(/[<>]/g, '').replace(/\s+/g, ' ').trim();
-                        const yearInput = parseInt(year.getValue());
-                        const ratingInput = parseFloat(rating.getValue());
-                        const votesInput = parseInt(votes.getValue());
-                        
-                        $$("filmsTable").add({
-                            title: titleInput,
-                            year: yearInput,
-                            rating: ratingInput,
-                            votes: votesInput
-                        });
-
+                    if (form.validate()) {                
+                        $$("filmsTable").add(form.getValues());
                         form.clear();
                         webix.message({
                             text: "Your film is successfully added to the list!",
@@ -111,21 +205,16 @@ const editFilmsForm = {
                         }
                     );
                 }
-             }
-        ],
+            }
+            ],
             margin: 15
         }
     ],
-    rules:{
-        title: webix.rules.isNotEmpty,
-        year: value => {
-            return Number.isInteger(value) && (value >= 1970) && (value <= new Date().getFullYear());
-        },
-        rating: value => {
-            return webix.rules.isNumber(value) && value != 0;   
-        },
-        votes: value => {
-            return Number.isInteger(value) && (value > 0) && (value < 100000);
+    elementsConfig:{
+        on:{
+           'onTimedKeyPress'() {
+                this.validate();
+            }
         }
     }
 }
