15e96263130c5bd304cd727182e73613677a48e5
EkaterinaPoddubskaya <kpoddubskaya@xbsoftware>
2024-08-20 21:10:58 +0300

    bind form to datatable, add tabbar, add filter by tabbar

---
+++ data/filmsTabbar.js
@@ -0,0 +1,24 @@
+const FILMS_TAB_NAMES = {
+    ALL: "All", 
+    OLD: "Old", 
+    MODERN: "Modern", 
+    NEW: "New" 
+}
+
+const FILMS_TAB_IDS = {
+    ALL: "all", 
+    OLD: "old", 
+    MODERN: "modern", 
+    NEW: "new" 
+}
+
+const getTabbarOption = (tabId, tabName) => {
+    return { id: tabId, value: tabName, width: 150 }
+}
+
+const tabbarOptions = [
+    getTabbarOption(FILMS_TAB_IDS.ALL, FILMS_TAB_NAMES.ALL),
+    getTabbarOption(FILMS_TAB_IDS.OLD, FILMS_TAB_NAMES.OLD),
+    getTabbarOption(FILMS_TAB_IDS.MODERN, FILMS_TAB_NAMES.MODERN),
+    getTabbarOption(FILMS_TAB_IDS.NEW, FILMS_TAB_NAMES.NEW),
+]
--- views/main.js
+++ views/main.js
@@ -56,14 +56,26 @@ const sideMenuList = {
     css: "side_menu"
 }
 
+const filmsYearTabbar = {
+    view:"tabbar",
+    id: "filmsTabbar",
+    options: tabbarOptions,
+    on: {
+        onAfterTabClick() {
+            $$("filmsTable").filterByAll();
+        }
+    }
+};
+
 let maxRank = 0;
 const filmLibraryTable = {
     view:"datatable", 
     id: "filmsTable",
     columns:[
-        { id: "rank", header: "", width: 50 , css: "film_rank_color"},
+        { id: "rank", header: "#", width: 50 , css: "film_rank_color", sort: "int" },
         { id: "title", header: ["Film Title", {content: "textFilter"}], sort: "string", fillspace: true, css: "left_align_text" },
-        { id: "year", header: ["Released", {content: "textFilter"}], sort: "int" },
+        { id: "category", header: ["Category", {content: "selectFilter"}], collection: "data/categories.js", sort: "text" },
+        { id: "year", header: "Released", sort: "int" },
         { id: "votes", header: ["Votes", {content: "textFilter"}], sort: "int" },
         { id: "rating", header:["Rating", {content: "textFilter"}], sort: "int" },
         { template:"<span class='webix_icon wxi-trash delete_film'></span>", css: "icon_color_hover" }
@@ -81,18 +93,12 @@ const filmLibraryTable = {
             if(!webix.rules.isNumber(obj.rating)) {
                 obj.rating = parseFloat(obj.rating.replace(",", "."));
             }
+            obj.category = Math.floor(Math.random() * 4) + 1;
         }
     },
-    editable: true,
-    form: "filmsForm",
-    onClick:{
+    onClick: {
         "delete_film"(e, id) {
             this.remove(id);
-            const form = $$(this.config.form);
-            if (form.getValues().id === id.row) {
-                form.clear();
-                form.clearValidation();
-            }
             return false;
         }
     },
@@ -104,7 +110,14 @@ const filmLibraryTable = {
     select: true,
     css: "centered_text",
     hover: "film_hover",
-    scroll: "y",
+    scroll: "y"
+}
+
+const filmsLibrary = {
+    rows: [
+        filmsYearTabbar, 
+        filmLibraryTable
+    ],
     gravity: 3
 }
     
@@ -149,13 +162,12 @@ const editFilmsForm = {
                     const form = $$("filmsForm");
                     if (form.validate()) {
                         let messageText = "";
-                        const table = $$("filmsTable");
                         const formId = form.getValues().id; 
                         if (formId) {
-                            table.updateItem(formId, form.getValues());
+                            form.save();
                             messageText = "Your changes are successfully saved!";
                         } else {
-                            table.add(form.getValues());
+                            form.save();
                             messageText = "Your film is successfully added to the list!";
                         }
                         form.clear();
@@ -188,8 +200,15 @@ const editFilmsForm = {
     ],
     elementsConfig:{
         on:{
-           onTimedKeyPress() {
+            onTimedKeyPress() {
                 this.validate();
+            }      
+        }
+    },
+    on: {
+        onBindApply() {
+            if (Object.values(this.getValues()).every(v => !v)) {
+                this.clearValidation();
             }
         }
     }
@@ -203,8 +222,8 @@ const footer = {
 
 const multiview = {
     cells: [
-        { id: MULTIVIEW_IDS.FILMS_VIEW_ID, cols: [filmLibraryTable, editFilmsForm] },
-        { id: MULTIVIEW_IDS.USERS_VIEW_ID, rows: [ usersToolbar, usersList, usersChart] },
+        { id: MULTIVIEW_IDS.FILMS_VIEW_ID, cols: [ filmsLibrary, editFilmsForm ] },
+        { id: MULTIVIEW_IDS.USERS_VIEW_ID, rows: [ usersToolbar, usersList, usersChart ] },
         { id: MULTIVIEW_IDS.PRODUCTS_VIEW_ID, rows: [ productsTable ] },
         { id: MULTIVIEW_IDS.ADMINS_VIEW_ID }
     ]
@@ -214,7 +233,7 @@ webix.ready(() => {
     webix.ui({
         rows: [
             header,
-            {cols: [
+            { cols: [
                 sideMenuList,
                 { view:"resizer" },
                 multiview
@@ -222,4 +241,23 @@ webix.ready(() => {
             footer
         ]
     });
+
+    const table = $$("filmsTable");
+    $$("filmsForm").bind(table);
+
+    table.registerFilter(
+        $$("filmsTabbar"),
+        { 
+            columnId: "year",
+            compare(cellValue, tabViewId) {
+                switch(tabViewId) {
+                    case FILMS_TAB_IDS.ALL: return cellValue;
+                    case FILMS_TAB_IDS.OLD: return cellValue < 1970;
+                    case FILMS_TAB_IDS.MODERN: return cellValue >= 1970 && cellValue < 2000;
+                    case FILMS_TAB_IDS.NEW: return cellValue >= 2000;
+                }
+            }
+        },
+        { getValue: (tabView) => tabView.getValue() }
+    );
 });
